2fab57c2ce5f0773bead76fb8a066f37eb3b7a40
Ravensd7922 <johan_reinoso@ucne.edu.do>
2025-10-01 21:01:52 -0400

    Entrada model creada

---
+++ app/src/main/java/com/example/johan_reinoso_ap2_p1/Domain/model/Entrada.kt
@@ -0,0 +1,10 @@
+package com.example.johan_reinoso_ap2_p1.Domain.model
+
+class Entrada (
+    val IdEntrada: Int,
+    val NombreCliente: String,
+    val Cantidad: Int,
+    val Precio: Double,
+    val Fecha: String
+){
+}
